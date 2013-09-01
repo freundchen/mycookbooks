ec93d4480388f4e4c9997817f47a22c9c77fe449
Enno Brehm <brehm@amazon.com>
2013-09-01 16:03:48 +0200

    Add redis configuration template.

--- elasticache-redis/recipes/deploy.rb
+++ elasticache-redis/recipes/deploy.rb
@@ -1,26 +1,30 @@
-#
-# Cookbook Name:: helloworld
-# Recipe:: default
-#
-# Copyright 2012, Jonathan Klinginsmith
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+include_recipe "deploy"
 
-# If a message was provided then display it; otherwise, say 'Hello World'.
-message = node.has_key?(:message) ? node[:message] : "Hello World"
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
 
-execute "echo message" do
-  command "echo ============================ '#{message}'"
-  action :run
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/redis.yml" do
+    source "redis.yml.erb"
+    cookbook 'elasticache-redis'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(
+      :redis => deploy[:redis] || {},
+      :environment => deploy[:rails_env]
+    )
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
 end
+
