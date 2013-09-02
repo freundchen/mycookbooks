a6e182734649f09ff15f2129a688de2eb47f9be0
Enno Brehm <brehm@amazon.com>
2013-09-03 01:37:27 +0200

    Rename and fix cookbook.

---
+++ redis-config/recipes/deploy.rb
@@ -0,0 +1,29 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  # use template ‘redis.yml.erb’ to generate configuration file
+  template "#{deploy[:deploy_to]}/current/config/redis.yml" do
+    source "redis.yml.erb"
+    cookbook "redis-config"
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    # define variable “@redis” to be used in the ERB template
+    variables(
+      :redis => deploy[:redis] || {},
+    )
+
+    # ensure the application is restarted after the configuration
+    # is generated
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    # generate the file only if there actually is a config folder
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/current/config/")
+    end
+  end
+end
+
+
